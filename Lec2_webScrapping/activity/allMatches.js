891620e9f708687b892a96d8c5a4f06380cc0aaa
sushberiwal <sushantberiwal@gmail.com>
2020-10-05 15:55:46 +0530

    got all details of matches

--- Lec2_webScrapping/activity/allMatches.js
+++ Lec2_webScrapping/activity/allMatches.js
@@ -1,6 +1,7 @@
 let fs = require("fs");
 let request = require("request");
 let cheerio = require("cheerio");
+const getMatch = require("./match");
 
 
 function getAllMatches(link){
@@ -19,7 +20,8 @@ function parseData(data){
     for(let i=0 ; i<allATags.length ; i++){
         let link = ch(allATags[i]).attr("href");
         let completeLink =  "https://www.espncricinfo.com"+link;
-        console.log(completeLink);
+        // console.log(completeLink);
+        getMatch(completeLink);
     }
 
 }
--- Lec2_webScrapping/activity/match.js
+++ Lec2_webScrapping/activity/match.js
@@ -3,11 +3,15 @@ let cheerio = require("cheerio");
 let request = require("request");
 
 
-request("https://www.espncricinfo.com/series/8039/scorecard/1144529/england-vs-australia-2nd-semi-final-icc-cricket-world-cup-2019" , fun);
+function getMatch(link){
+    request(link , fun);
+
+}
 
 function fun(error , res , data){
     parseData(data);
 }
+
 
 function parseData(data){
     let ch = cheerio.load(data);
@@ -19,6 +23,27 @@ function parseData(data){
         // ["Australia" , "(50 overs maximum)" ];
         teamName = teamName.split("Innings")[0].trim();
         console.log(teamName);
+        // array of tr [  <tr> </tr> , <tr> </tr> , <tr> </tr> , <tr> </tr> , <tr> </tr> , <tr> </tr> , <tr> </tr>  ];
+        let allBatsmanDetails = ch(bothInnings[i]).find(".table.batsman tbody tr");
+        for(let j=0 ; j<allBatsmanDetails.length-1 ; j++){
+            let detailsOfABatsman = ch(allBatsmanDetails[j]).find("td");
+            if(detailsOfABatsman.length > 1){
+                // valid batsman ki details
+                let batsmanName = ch(detailsOfABatsman[0]).find("a").text().trim();
+                let runs = ch(detailsOfABatsman[2]).text().trim();
+                let balls = ch(detailsOfABatsman[3]).text().trim();
+                let fours = ch(detailsOfABatsman[5]).text().trim();
+                let sixes = ch(detailsOfABatsman[6]).text().trim();
+                let strikeRate = ch(detailsOfABatsman[7]).text().trim();
+                // String interpolation
+                console.log(`Batsman = ${batsmanName} Balls = ${balls} Runs = ${runs} Fours = ${fours} Sixes = ${sixes} SR = ${strikeRate} `);
+            }
+        }
 
     }
+    console.log("#######################################");
 }
+
+
+
+module.exports = getMatch;
