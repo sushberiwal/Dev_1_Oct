45289895ae55dd2bfd1b20a1db02924b0aacb57b
sushberiwal <sushantberiwal@gmail.com>
2020-10-08 16:34:59 +0530

    logged in !

--- Lec4_Automation/activity/hackerRank_CodeSubmission.js
+++ Lec4_Automation/activity/hackerRank_CodeSubmission.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
-
+const {id,pw} = require("./credentials");
     // puppeteer functions => pending promise 
-
+    let tab;
     // gives a browser instance
     let browserPromise = puppeteer.launch({
-        headless:false
+        headless:false,
+        defaultViewport:null,
+        args:["--start-maximized"]
     });
     browserPromise.then(function(browser){
         let allPagesPromise = browser.pages();
@@ -12,13 +14,28 @@ const puppeteer = require('puppeteer');
     })
     .then(function(pages){
         let page = pages[0];
-        let pageOpenedPromise = page.goto("https://www.google.com");
+        tab = page;
+        let pageOpenedPromise = page.goto("https://www.hackerrank.com/auth/login");
         return pageOpenedPromise
     })
     .then(function(){
-        console.log("google opened !");
+        let idTypedPromise = tab.type("#input-1" , id);
+        return idTypedPromise;
     })
-
+    .then(function(){
+        let idTypedPromise = tab.type("#input-2" , pw);
+        return idTypedPromise;
+    })
+    .then(function(){
+        let loginPromise = tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button");
+        return loginPromise;
+    })
+    .then(function(){
+        console.log("logged in !!");
+    })
     .catch(function(err){
         console.log(err);
     })
+
+
+
